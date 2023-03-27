cba74cdfe12ccadf05ed1943ab6a4540581251c6
Christos Kokas <christos.kokas95@gmail.com>
2023-03-27 21:24:30 +0300

    change build

--- src/vio_slam/build.sh
+++ src/vio_slam/build.sh
@@ -1,13 +1,27 @@
 echo "building Thirdparty/ceres-solver.2-1-0 ..."
 
+sudo apt-get install cmake
+
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+
+sudo apt-get install libatlas-base-dev
+
+sudo apt-get install libeigen3-dev
+
+sudo apt-get install libsuitesparse-dev
+
 cd Thirdparty/ceres-solver-2.1.0
 mkdir build
 cd build
-cmake ../ceres-solver-2.1.0
+cmake ..
 make -j3
 make install
 
 cd ../../../
+
+sudo apt install libopencv-dev python3-opencv
+
+sudo apt-get install python3-catkin-tools
 
 mkdir build
 cd build
